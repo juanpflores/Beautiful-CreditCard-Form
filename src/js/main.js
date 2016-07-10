62072b1a2244f5bda73d47175347b663d16c8123
Juan Pablo <juanpflores94@gmail.com>
2016-07-10 17:55:52 -0500

    copy data to card done. Closes #2

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,37 @@ $(document).ready(function() {
 	$('.credit-card').addClass("neutral");
 
 });
+
+// Copy text to CC Number
+$('#cc-number').keyup(function(){
+	var creditcard_number = $(this).val();
+	console.log(creditcard_number);
+	$('#credit-card-number').text(creditcard_number);
+});
+
+// Copy Text to Name
+$('#name-input').keyup(function(){
+	var name = $(this).val();
+	console.log(name);
+	$('#card-holder').text(name);
+});
+
+// Copy Text to CVC
+$('#cvc-input').keyup(function(){
+	var cvc = $(this).val();
+	console.log(cvc);
+	$('#cvc').text(cvc);
+});
+
+// Copy Text to Month and Year
+$('#month').change(function(){
+	var month = $(this).val();
+	console.log(month);
+	$('#month-card').text(month);
+});
+
+$('#year').change(function(){
+	var year = $(this).val();
+	console.log(year);
+	$('#year-card').text(year);
+});
